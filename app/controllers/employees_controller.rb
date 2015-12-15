692edc2e87b23e338b1101c0265859d6e927cd46
Peter Jang <peter.x.jang@gmail.com>
2015-12-14 20:45:19 -0600

    added create action

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -6,4 +6,13 @@ class EmployeesController < ApplicationController
   def show
     @employee = Employee.find_by(id: params[:id])
   end
+
+  def create
+    @employee = Employee.create(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email]
+    )
+    render :show
+  end
 end
